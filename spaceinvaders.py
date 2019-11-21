dd4d415df607a701acf0719d83ba9845018fe8e3
hzhao22 <hzhao2@gmail.com>
2019-11-20 21:57:58 -0500

    2019.11.20 game start

---
+++ spaceinvaders.py
@@ -0,0 +1,36 @@
+import pygame
+
+#initialize the pygame
+pygame.init()
+
+# create the screen
+screen = pygame.display.set_mode((800,600))
+
+#Title and Icon
+pygame.display.set_caption("Space Invaders")
+
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
+#Player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
+
+# Game Loop
+running = True
+while running:
+
+    # RGB - Red, Green, BLue
+    screen.fill((255, 255, 255))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    player()
+    pygame.display.update()
