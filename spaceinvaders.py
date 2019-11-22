8337222eca736f2df88b01ef089ce26b8699a187
hzhao22 <hzhao2@gmail.com>
2019-11-21 21:50:51 -0500

    2019.11.21 game start

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -1,24 +1,36 @@
 import pygame
 
-#initialize the pygame
+# initialize the pygame
 pygame.init()
 
 # create the screen
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((800, 600))
 
-#Title and Icon
+# Title and Icon
 pygame.display.set_caption("Space Invaders")
 
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
-#Player
+# Player
 playerImg = pygame.image.load('player.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+# Enemy
+enemyImg = pygame.image.load('enemy.png')
+enemyX = 370
+enemyY = 50
+enemyX_change = 0
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
 
 
 # Game Loop
@@ -26,11 +38,32 @@ running = True
 while running:
 
     # RGB - Red, Green, BLue
-    screen.fill((255, 255, 255))
+    screen.fill((105, 205, 255))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    player()
+    # if keystroke is pressed check whether its right or left
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            playerX_change = -0.3
+        if event.key == pygame.K_RIGHT:
+            playerX_change = 0.3
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            playerX_change = 0
+
+    # 5 = 5 + -0.1 -> 5 = 5 - 0.1
+    # 5 = 5 + 0.1
+
+    playerX += playerX_change
+
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+
+    player(playerX, playerY)
+    enemy(enemyX, enemyY)
     pygame.display.update()
